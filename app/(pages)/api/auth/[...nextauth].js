e662836cd74bc5e38ff9b20c3661a867418c412f
ajay sharma <ajaysharma445446@gmail.com>
2025-04-10 18:57:28 +0530

    changes

--- app/(pages)/api/auth/[...nextauth].js
+++ app/(pages)/api/auth/[...nextauth].js
@@ -11,14 +11,14 @@ export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async session({ session, token }) {
-        console.log("Session Callback:", session, token);
-        session.user.id = token.sub;
-        return session;
-      },
-      async jwt({ token, user }) {
-        console.log("JWT Callback:", token, user);
-        if (user) token.id = user.id;
-        return token;
-      },
+      console.log("Session Callback:", session, token);
+      session.user.id = token.sub;
+      return session;
+    },
+    async jwt({ token, user }) {
+      console.log("JWT Callback:", token, user);
+      if (user) token.id = user.id;
+      return token;
+    },
   },
 });
